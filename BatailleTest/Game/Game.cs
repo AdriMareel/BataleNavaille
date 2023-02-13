04e629dc8e9ee50ac78c6f84469126e7ccc9f09c
POTEZ <60687263+P0TEZ@users.noreply.github.com>
2023-02-13 15:16:58 +0100

    Update Game.cs

--- BatailleTest/Game/Game.cs
+++ BatailleTest/Game/Game.cs
@@ -11,6 +11,9 @@ using Windows.UI.Xaml.Documents;
 
 namespace BatailleTest.Game
 {
+    ///-------------------------------------------------------------------------------------------------
+    /// <summary>   Classe game. </summary>
+
     internal class Game
     {
         private GameRules _gameRules;
@@ -29,6 +32,12 @@ namespace BatailleTest.Game
 
         private int _turn;
 
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Constructor. </summary>
+        ///
+        /// <param name="player1Name">  (Optional) Name of the player 1. </param>
+        /// <param name="player2Name">  (Optional) Name of the player 2. </param>
+        /// <param name="gameRules">    (Optional) The game rules. </param>
 
         public Game(string player1Name = "Player1", string player2Name = "Player2", GameRules gameRules = null)
         {
@@ -46,40 +55,80 @@ namespace BatailleTest.Game
             _gameState = new GameStates(GameStates.States.Warmup);
         }
 
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets the player 1. </summary>
+        ///
+        /// <value> The player 1. </value>
+
         public Player Player1
         {
             get { return _player1; }
         }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets the player 2. </summary>
+        ///
+        /// <value> The player 2. </value>
 
         public Player Player2
         {
             get { return _player2; }
         }
 
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets the game rules. </summary>
+        ///
+        /// <value> The game rules. </value>
+
         public GameRules GameRules
         {
             get { return _gameRules; }
         }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets the turn. </summary>
+        ///
+        /// <value> The turn. </value>
 
         public int Turn
         {
             get { return _turn; }
         }
 
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets the current player. </summary>
+        ///
+        /// <value> The current player. </value>
+
         public Player CurrentPlayer
         {
             get { return _currentPlayer; }
         }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets the other player. </summary>
+        ///
+        /// <value> The other player. </value>
 
         public Player OtherPlayer
         {
             get { return _otherPlayer; }
         }
 
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets the player one board. </summary>
+        ///
+        /// <value> The player one board. </value>
+
         public Board PlayerOneBoard
         {
             get { return _playerOneBoard; }
         }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets the player two board. </summary>
+        ///
+        /// <value> The player two board. </value>
 
         public Board PlayerTwoBoard
         {
@@ -90,6 +139,9 @@ namespace BatailleTest.Game
         {
             //TODO
         }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Switch the current player with the other player. </summary>
 
         private void SwitchPlayer()
         {
@@ -105,11 +157,25 @@ namespace BatailleTest.Game
             }
         }
 
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Next turn add 1 turn. </summary>
+
+
         private void NextTurn()
         {
             _turn++;
             SwitchPlayer();
         }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Play turn. </summary>
+        ///
+        /// <exception cref="Exception">    Thrown when an exception error condition occurs. </exception>
+        ///
+        /// <param name="coordinates">  The coordinates where the player is playing. </param>
+        /// <param name="player">       (Optional) The player (needed only during warmup). </param>
+        ///
+        /// <returns>   An int. </returns>
 
         public int PlayTurn(Coordinates coordinates, Player player = null)
         {
@@ -187,7 +253,15 @@ namespace BatailleTest.Game
                 throw e;
             }
         }
-        
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Adds a ship to a player. </summary>
+        ///
+        /// <param name="player">   The player who is placing the ship. </param>
+        /// <param name="ship">     The ship. </param>
+        ///
+        /// <returns>   True if it succeeds, false if it fails. </returns>
+
         private bool AddAShip(Player player, Ship ship)
         {
             GameRules gameRules = this.GameRules;
@@ -201,6 +275,13 @@ namespace BatailleTest.Game
                 return false;
             }
         }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Adds a shot. </summary>
+        ///
+        /// <param name="coordinates">  The coordinates where the player is playing. </param>
+        ///
+        /// <returns>   True if it succeeds, false if it fails. </returns>
 
         private bool AddAShot(Coordinates coordinates)
         {
@@ -223,6 +304,11 @@ namespace BatailleTest.Game
             return true;
         }
 
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Check if this game is over. </summary>
+        ///
+        /// <returns>   True if over, false if not. </returns>
+
         public bool IsOver()
         {
             if (this.GetWinner() != null)
@@ -234,6 +320,11 @@ namespace BatailleTest.Game
                 return false;
             }
         }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Return the game winner. </summary>
+        ///
+        /// <returns>   The winner. </returns>
 
         public Player GetWinner()
         {
